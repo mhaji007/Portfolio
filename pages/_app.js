e7b86d571d089734288afd4cad461197f78c5f89
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-27 14:17:06 -0400

    Move initialProps up to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.scss';
 
 
-const App = ({Component, pageProps}) => {
+class App extends Component {
 
+    static async getInitialProps({Component, ctx}){
+
+        let pageProps = {};
+
+        if(Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+    
+        return {pageProps};
+    
+      }
+
+
+render() {
+
+    const{ Component, pageProps} = this.props;
     return <Component {...pageProps}/>
+
+}
+
 }
 
 export default App;
