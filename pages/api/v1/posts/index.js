756df93ba46f257a17a27b8ed5022d05041e77c2
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-01 22:12:59 -0400

    Refactor the Api function. Remove getInititalProps from index

--- pages/api/v1/posts/index.js
+++ pages/api/v1/posts/index.js
@@ -1,8 +1,18 @@
+import axios from 'axios';
 
-import {data} from '@/data';
+async function handlePosts (req, res) {
 
-function handlePosts (req, res) {
-    res.status(200).json(data)
+  try {
+
+    const axiosRes = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const posts = axiosRes.data;
+    res.status(200).json(posts.slice(0,10));
+
+  } catch(e) {
+    console.error(e);
+    res.status(error.status || 400).end('Api Error');
+  }
+
 }
 
 export default handlePosts;
