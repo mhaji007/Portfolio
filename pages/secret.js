b91a900b74f30dc93f9ec3a565842c07f72ef655
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-04 21:49:11 -0400

    Set up secret page. Restrict access to private resources

---
+++ pages/secret.js
@@ -0,0 +1,33 @@
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
+import {useGetUser} from '@/actions/user';
+import {useRouter} from 'next/router';
+
+const Secret = () => {
+    const {data, loading} = useGetUser();
+    const router = useRouter();
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+    if (!data && typeof window !== 'undefined') {
+        //TODO: Improve return
+        router.push('/api/v1/login');
+        return null;
+    } else {
+
+        return (
+            <BaseLayout user={data} loading={loading}>
+                <BasePage>
+                    <h1>
+                        I am Secret page
+                    </h1>
+                </BasePage>
+            </BaseLayout>
+        )
+
+    }
+
+}
+
+export default Secret;
