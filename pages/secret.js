17ef1806c28f73ff3b8f95a1072f384a29c490e0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-04 22:00:17 -0400

    Refactor redirect to make it reusable

--- pages/secret.js
+++ pages/secret.js
@@ -1,19 +1,21 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import {useGetUser} from '@/actions/user';
-import {useRouter} from 'next/router';
+// import {useRouter} from 'next/router'; if not using the custom redirect
+// component
+import Redirect from '@/components/shared/Redirect';
 
 const Secret = () => {
     const {data, loading} = useGetUser();
-    const router = useRouter();
+    // const router = useRouter();  if not using the custom redirect component
 
     if (loading) {
         return <p>Loading...</p>
     }
     if (!data && typeof window !== 'undefined') {
-        //TODO: Improve return
-        router.push('/api/v1/login');
-        return null;
+        // TODO: Improve return router.push('/api/v1/login'); return null;
+        return <Redirect to="/api/v1/login"/>
+
     } else {
 
         return (
