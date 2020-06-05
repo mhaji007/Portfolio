dfe9a1ca8e8c8503535d591bbea9ccd4aa28aff0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-05 16:37:37 -0400

    Implement server-side redirect/render functionality

---
+++ pages/secretssr.js
@@ -0,0 +1,55 @@
+import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
+import auth0 from '@/utils/auth0';
+
+const SecretSSR = ({user}) => {
+    
+        return (
+            <BaseLayout user={user} loading={false}>
+                <BasePage>
+                    <h1>
+                        I am Secret page - {user && user.name}
+                    </h1>
+                </BasePage>
+            </BaseLayout>
+        )
+
+    }
+
+    // Server-side authentication
+    export const getServerSideProps = async ({req, res}) => {
+        const session = await auth0.getSession(req);
+        if(!session ||!session.user) {
+            res.writeHead(302, {
+                location:'/api/v1/login'
+            });
+            res.end();
+            return{props:{}};
+        }
+        return {
+            props:{user:session.user}
+        }
+    }
+
+    export default SecretSSR;
+
+
+
+
+
+// traditional syntax
+
+// function withAuth(Component) {
+//     return function (props) {
+//         return <Component title="" {...props}/>
+//     }
+
+
+// }
+
+// // arrow function syntax
+// const withAuth = (Component) =props => <Component title="" {...props}/>
+    
+
+
+
