43440af251838a56649fc1490d8ec5fd7dbce3f8
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-06 14:15:17 -0400

    Add getServerSideProps

--- pages/secretssr.js
+++ pages/secretssr.js
@@ -1,15 +1,17 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
-import {authorizedUser} from '@/utils/auth0';
+import {authorizedUser, withAuth} from '@/utils/auth0';
 
-const SecretSSR = ({user}) => {
-    
+const SecretSSR = ({user, title}) => {
         return (
             <BaseLayout user={user} loading={false}>
                 <BasePage>
                     <h1>
                         I am Secret page - {user && user.name}
                     </h1>
+                    <h2>
+                        {title}
+                    </h2>
                 </BasePage>
             </BaseLayout>
         )
@@ -18,12 +20,12 @@ const SecretSSR = ({user}) => {
 
 
     // Server-side redirect
-    export const getServerSideProps = async ({req, res}) => {
-        const user = await authorizedUser(req, res);
+    // export const getServerSideProps = async ({req, res}) => {
+    //     const user = await authorizedUser(req, res);
 
-        return {
-            props: {user}
-        }
+    //     return {
+    //         props: {user}
+    //     }
         
 
         
@@ -38,7 +40,20 @@ const SecretSSR = ({user}) => {
         // return {
         //     props:{user:session.user}
         // }
+    // }
+
+    const getTitle = () => {
+        return new Promise((res) => {
+          setTimeout(() => {
+            res({title: 'My new title!'})
+          }, 500)
+        })
     }
+
+    export const getServerSideProps = withAuth(async ({req, res}, user) => {
+        const title = await getTitle();
+        return title;
+      });
 
     export default SecretSSR;
 
