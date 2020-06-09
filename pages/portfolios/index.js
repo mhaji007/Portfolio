a0731d3aeb919e23df632177e0543bec226fa952
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-09 17:02:48 -0400

    Create portfolio card

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -3,31 +3,38 @@ import BasePage from '@/components/BasePage';
 import Link from 'next/link';
 import {useGetUser} from '@/actions/user';
 import PortfolioApi from '@/lib/api/portfolios';
+import PortfolioCard from '@/components/PortfolioCard';
 
+import {Row, Col } from 'reactstrap';
 const Portfolios = ({portfolios}) => {
   
    const {data:dataU, loading:loadingU} = useGetUser();
 
-   const renderPortfolios = (portfolios) => {
-    return portfolios.map(portfolio =>
-      <li key={portfolio._id} style={{'fontSize': '20px'}}>
-        <Link as={`/portfolios/${portfolio._id}`} href="/portfolios/[id]">
-                    <a>
-                        {portfolio.title}
-                    </a>
-                </Link>
-            </li>
-        )
-    }
+  //  const renderPortfolios = (portfolios) => {
+  //   return portfolios.map(portfolio =>
+  //     <li key={portfolio._id} style={{'fontSize': '20px'}}>
+  //       <Link as={`/portfolios/${portfolio._id}`} href="/portfolios/[id]">
+  //                   <a>
+  //                       {portfolio.title}
+  //                   </a>
+  //               </Link>
+  //           </li>
+  //       )
+  //   }
 
     return (
         <BaseLayout
         user ={dataU}
         loading = {loadingU}>
-            <BasePage>
-            <ul>
-          {renderPortfolios(portfolios)}
-            </ul>
+            <BasePage className="portfolio-page">
+            <Row>
+              {portfolios.map(portfolio =>
+              <Col key={portfolio._id} md="4">
+                  <PortfolioCard portfolio={portfolio}/>
+              </Col>
+                    )
+              }
+            </Row>
             </BasePage>
         </BaseLayout>
     )
