a2bc454c6f6612d81f122b40919387fb5dad0539
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-10 18:52:13 -0400

    Fetch portfolio details server-side

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,46 +1,38 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 //import {useGetData} from '@/actions'; // -  used if not using SWR
-import {useGetPostById} from '@/actions';
-import {useRouter} from 'next/router';
+//import {useGetPostById} from '@/actions';
 import {useGetUser} from '@/actions/user';
+import PortfolioApi from '@/lib/api/portfolios';
 
 
-const Portfolio = () => {
+const Portfolio = ({portfolio}) => {
     
-    const router = useRouter();
-    //const {data:portfolio, error, loading} = 
+    //const {data:portfolio, error, loading} = useGetUser(); 
     //useGetData(router.query.id?`/api/v1/posts/${router.query.id}`:null); // -  used if not using SWR
-    const {data: portfolio, error, loading} =
-    useGetPostById(router.query.id);
-    const {data:dataU, loading:loadingU} = useGetUser();
+    //const {data: portfolio, error, loading} =
+    // useGetPostById(router.query.id);
+   const {data:dataU, loading:loadingU} = useGetUser();
     
     return (
         <BaseLayout
         user ={dataU}
         loading = {loadingU}>
-            <BasePage>
-                {loading && <p>Loading Data...</p>}
-                {error && <div className="alert alert-danger">{error.message}</div>}
-                {portfolio &&
-                <>
-                <h1>
-                    I am Portfolio page
-                </h1>
-                <h1>
-                    {portfolio.title}
-                </h1>
-                <p>BODY: {portfolio.body}
-                </p>
-                <p>ID: {portfolio.id}
-                </p>
-                </>
-}
+            <BasePage header = "Portfolio Details">
+                {
+                    JSON.stringify(portfolio)
+                }
+
             </BasePage>
         </BaseLayout>
     );
 }
 
+export async function getServerSideProps({query}) {
+    const json = await new PortfolioApi().getById(query.id);
+    const portfolio = json.data;
 
+    return {props:{portfolio}};
+}
 
 export default(Portfolio);
