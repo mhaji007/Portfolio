5fba96618c4f3f78711ed087e4b929a1378ffd69
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-10 23:38:52 -0400

    Fetch portfolio details using getServerSideProps

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -8,9 +8,9 @@ import PortfolioApi from '@/lib/api/portfolios';
 
 const Portfolio = ({portfolio}) => {
     
-    //const {data:portfolio, error, loading} = useGetUser(); 
-    //useGetData(router.query.id?`/api/v1/posts/${router.query.id}`:null); // -  used if not using SWR
-    //const {data: portfolio, error, loading} =
+    // const {data:portfolio, error, loading} = useGetUser(); 
+    // useGetData(router.query.id?`/api/v1/posts/${router.query.id}`:null); // -  used if not using SWR
+    // const {data: portfolio, error, loading} =
     // useGetPostById(router.query.id);
    const {data:dataU, loading:loadingU} = useGetUser();
     
@@ -28,11 +28,40 @@ const Portfolio = ({portfolio}) => {
     );
 }
 
-export async function getServerSideProps({query}) {
-    const json = await new PortfolioApi().getById(query.id);
-    const portfolio = json.data;
 
+
+// // Fetch portfolio details serverside
+
+// export async function getServerSideProps({query}) {
+//     const json = await new PortfolioApi().getById(query.id);
+//     const portfolio = json.data;
+
+//     return {props:{portfolio}};
+// }
+
+
+// Executed at build time
+// Prerenders dynamic pages
+export async function getStaticPaths() {
+    const json = await new PortfolioApi().getAll();
+    const portfolios = json.data;
+
+    // Get paths we want to pre-render based on portfolio
+    // array of objects [{params:{id: '5eba7a1510e66775743ae598},{params:{id: '5eba7a1510e66775743ae598}, {params:{id: '5eba7a1510e66775743ae598} }]
+    const paths = portfolios.map(portfolio => {
+        return {
+            params: {id: portfolio._id}
+        }
+    })
+    // fallback: false means that 'not found' pages will resolve into 404 page
+    return {paths, fallback:false};
+}
+
+export async function getStaticProps({params}) {
+    const json = await new PortfolioApi().getById(params.id);
+    const portfolio = json.data;
     return {props:{portfolio}};
 }
 
-export default(Portfolio);
+
+export default Portfolio;
