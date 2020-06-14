08d7f6c96e945cadba7090b5991de970919fd6fa
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-14 18:35:10 -0400

    Prefill form data

--- components/PortfolioForm.js
+++ components/PortfolioForm.js
@@ -3,15 +3,23 @@ import DatePicker from 'react-datepicker';
 import {useState, useEffect} from 'react';
 import {useForm} from 'react-hook-form';
 
-const PortfolioForm = ({onSubmit}) => {
+// const PortfolioForm = ({onSubmit}) => {
+    const PortfolioForm = ({onSubmit, initialData = {}}) => {
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
-    const {register, handleSubmit, setValue} = useForm();
+    //const {register, handleSubmit, setValue} = useForm();
+    const { register, handleSubmit, setValue } = useForm({defaultValues: initialData});
 
-    useEffect(()=> {
-        register({name:'startDate'});
-        register({name: 'endDate'});
-    }, [register])
+    useEffect(() => {
+        const { startDate, endDate } = initialData;
+        if (startDate) { setStartDate(new Date(startDate))}
+        if (endDate) { setEndDate(new Date(endDate))}
+      }, [initialData])
+
+    // useEffect(()=> {
+    //     register({name:'startDate'});
+    //     register({name: 'endDate'});
+    // }, [register])
 
     // const handleStartDate = (date) => {
     //     setStartDate(date);
