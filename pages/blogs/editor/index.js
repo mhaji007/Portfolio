764e7f726e14dbcc6cdcc527d964dc81629e8649
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-22 14:48:22 -0400

    Set up blog editor

--- pages/blogs/editor/index.js
+++ pages/blogs/editor/index.js
@@ -1,12 +1,19 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import withAuth from '@/components/hoc/withAuth';
+import {Editor} from 'slate-simple-editor';
 
 const BlogEditor = ({user, loading}) => {
+
+  const saveBlog = (data) => {
+    console.log(data);
+  }
+
+
   return (
     <BaseLayout user={user} loading={loading}>
-      <BasePage header="Blog Editor">
-        <h1>Some text</h1>
+      <BasePage>
+        <Editor header="Blog away!" onSave={saveBlog}/>
       </BasePage>
     </BaseLayout>
   )
