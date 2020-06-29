9ff64035c0dc0e14ed3c8a9f130020ddd5a950e9
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-29 13:54:20 -0400

    Provide default content to blog update

--- pages/blogs/editor/[id].js
+++ pages/blogs/editor/[id].js
@@ -3,16 +3,24 @@ import BasePage from '@/components/BasePage';
 import withAuth from '@/components/hoc/withAuth';
 import { Editor } from 'slate-simple-editor';
 import { toast } from 'react-toastify';
+import { useGetBlog } from 'actions/blogs';
+import { useRouter } from 'next/router';
 
 const BlogUpdateEditor = ({user, loading}) => {
+
+  const router = useRouter();
+  const { data } = useGetBlog(router.query.id);
 
   return (
     <BaseLayout user={user} loading={loading}>
       <BasePage>
-        <Editor
-          header="Update Your Blog..."
-          onSave={() => {}}
-        />
+      { data && data.content &&
+          <Editor
+            header="Update Your Blog..."
+            initialContent={data.content}
+            onSave={() => {}}
+          />
+        }
       </BasePage>
     </BaseLayout>
   )
