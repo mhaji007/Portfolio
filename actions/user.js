7837efaa3c50d5013c0689bc90a7e26e0b68be49
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-01 19:44:44 -0400

    Implement mutate function in dashbaord

--- actions/user.js
+++ actions/user.js
@@ -1,8 +1,7 @@
 import useSWR from 'swr';
-import {fetcher} from '@/actions';
+import { fetcher } from '@/actions';
 
 export const useGetUser = () => {
-    const {data, error, ...rest}  = useSWR('/api/v1/me', fetcher);
-    return {data, error, loading: !data && !error, ...rest};
-
+  const { data, error, ...rest} = useSWR('/api/v1/me', fetcher);
+  return { data, error, loading: !data && !error, ...rest};
 }
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,21 +1,25 @@
 import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
-//import withAuth from '@/components/hoc/withAuth';
-import { withAuth } from 'utils/auth0';
+import withAuth from '@/components/hoc/withAuth';
+//import { withAuth } from 'utils/auth0';
 import { Row, Col} from 'reactstrap';
 import Masthead from 'components/shared/Masthead';
 import PortDropdown from 'components/shared/Dropdown';
 import Link from 'next/link';
-import { useUpdateBlog } from 'actions/blogs';
+import { useUpdateBlog, useGetUserBlogs } from 'actions/blogs';
+import { toast } from 'react-toastify';
 import auth0 from 'utils/auth0';
 import BlogApi from 'lib/api/blogs';
 
-const Dashboard = ({user, blogs}) => {
+const Dashboard = ({user, loading}) => {
 
   const [updateBlog] = useUpdateBlog();
+  const {data: blogs, mutate} = useGetUserBlogs();
 
   const changeBlogStatus = async (blogId, status) => {
-    await updateBlog(blogId, {status});
+    updateBlog(blogId, {status})
+      .then(() => mutate())
+      .catch(() => toast.error('Something went wrong...'));
   }
 
   const createOption = (blogStatus) => {
@@ -38,7 +42,7 @@ const Dashboard = ({user, blogs}) => {
   }
   const renderBlogs = (blogs, status) => (
     <ul className="user-blogs-list">
-      { blogs.filter(blog => blog.status === status).map(blog =>
+     { blogs && blogs.filter(blog => blog.status === status).map(blog =>
         <li key={blog._id}>
           <Link href="/blogs/editor/[id]" as={`/blogs/editor/${blog._id}`}>
             <a>{blog.title}</a>
@@ -50,7 +54,7 @@ const Dashboard = ({user, blogs}) => {
     </ul>
   )
   return (
-    <BaseLayout navClass="transparent" user={user} loading={false}>
+    <BaseLayout navClass="transparent" user={user} loading={loading}>
       <Masthead imagePath="/images/background-index.PNG" />
       <BasePage className="blog-user-page">
         <Row>
@@ -68,11 +72,5 @@ const Dashboard = ({user, blogs}) => {
   )
 }
 
-//export default withAuth(Dashboard)('admin');
+export default withAuth(Dashboard)('admin');
 
-export const getServerSideProps = withAuth(async ({req, res}) => {
-  const { accessToken } = await auth0.getSession(req);
-  const json = await new BlogApi(accessToken).getByUser();
-  return { blogs: json.data }
-})('admin');
-export default Dashboard;
