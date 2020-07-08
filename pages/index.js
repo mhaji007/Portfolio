55d08bf65c060a722254859892cb46bf5e9f542e
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-08 18:17:12 -0400

    Provide base page to index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
 import BaseLayout from '@/components/layouts/BaseLayout';
+import BasePage from '@/components/BasePage';
 import {Container, Row, Col} from 'reactstrap';
 import Typed from 'react-typed';
 import {useGetUser} from '@/actions/user';
@@ -31,18 +32,19 @@ const Index = () => {
         navClass="transparent"
         //className={`cover ${isFlipping ? 'cover-orange' : 'cover-blue'}`}>
             className={`cover cover-blue`}>
-            <div className="main-section">
-                <div className="background-image">
-                    <img src="/images/background-index.png"/>
-                </div>
+                <BasePage indexPage>
+                
+                    <div className="main-section">
+                        <div className="background-image">
+                            <img src="/images/background-index.png"/>
+                        </div>
 
                 <Container>
                     <Row>
                         <Col md="6">
                             <div className="hero-section">
-                            <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
-                                    <div className="front">
-                                        
+                                <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
+                                    <div className="front">   
                                         <div className="hero-section-content">
                                             <h2>
                                                 Full Stack Web Developer
@@ -57,16 +59,19 @@ const Index = () => {
                                         </div>
                                     </div>
                                     <div className="back">
-                    <div className="hero-section-content">
-                      <h2> Full Stack Web Developer </h2>
-                      <div className="hero-section-content-intro">
-                      </div>
-                    </div>
-                    <img className="image" src="/images/section-2.JPG"/>
-                    <div className="shadow-custom shadow-custom-orange">
-                      <div className="shadow-inner"> </div>
-                    </div>
-                  </div>
+                                        <div className="hero-section-content">
+                                        <h2> Full Stack Web Developer 
+
+                                        </h2>
+                                        <div className="hero-section-content-intro">
+                                               {/* Have a look at my portfolio and job history */}
+                                        </div>
+                                    </div>
+                                        <img className="image" src="/images/section-2.JPG"/>
+                                        <div className="shadow-custom shadow-custom-orange">
+                                            <div className="shadow-inner"> </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </Col>
@@ -96,7 +101,8 @@ const Index = () => {
                         </Col>
                     </Row>
                 </Container>
-            </div>
+                    </div>
+            </BasePage>
         </BaseLayout>
     )
 }
