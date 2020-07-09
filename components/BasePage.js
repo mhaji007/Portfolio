fd89cb0fcba4aa69581922c82856275111391937
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-09 15:58:10 -0400

    Set up layout for portfolios detail

--- components/BasePage.js
+++ components/BasePage.js
@@ -2,10 +2,17 @@ import {Container} from 'reactstrap';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+const PageHeader = ({header}) =>
+  <div className="page-header">
+    <h1 className="page-header-title">{header}</h1>
+  </div>
+
+
 
 const BasePage = props => {
     const router = useRouter();
     const {
+      noWrapper,
         indexPage,
         className = '',
         header,
@@ -44,14 +51,27 @@ const BasePage = props => {
           href={`${process.env.BASE_URL}${canonicalPath ? canonicalPath : router.asPath}`} />
         </Head>
         <div className={`${pageType} ${className}`}>
-          <Container>
+          {/* <Container>
             { header &&
               <div className="page-header">
                 <h1 className="page-header-title">{header}</h1>
               </div>
             }
             {children}
+          </Container> */}
+
+{ noWrapper &&
+          <>
+            { header && <PageHeader header={header} /> }
+            {children}
+          </>
+        }
+        { !noWrapper &&
+          <Container>
+            { header && <PageHeader header={header} /> }
+            {children}
           </Container>
+        }
         </div>
       </>
     )
--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -16,15 +16,26 @@ const Portfolio = ({portfolio}) => {
     
     return (
         <BaseLayout
-        user ={dataU}
-        loading = {loadingU}>
+        navClass="transparent"
+        user={dataU} loading={loadingU}>
                   <BasePage
+                          noWrapper
+                          indexPage
         title={`${portfolio.title} - Mehdi Hajikhani`}
-        header="Portfolio Detail"
         metaDescription={portfolio.description}>
-                {
-                    JSON.stringify(portfolio)
-                }
+       <div className="portfolio-detail">
+          <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+            <main role="main" class="inner page-cover">
+              <h1 class="cover-heading">Title</h1>
+              <p class="lead dates">dates</p>
+              <p class="lead info mb-0">jobTitle | company | location</p>
+              <p class="lead">description</p>
+              <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary">Visit Company</a>
+              </p>
+            </main>
+          </div>
+        </div>
 
             </BasePage>
         </BaseLayout>
