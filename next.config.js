c118d757a004106c201d22d812b6449237cb698d
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-08 21:54:08 -0400

    Integrate open graph

--- next.config.js
+++ next.config.js
@@ -10,7 +10,8 @@ module.exports = {
     },
 
     env: {
-        AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE
+        AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
+        BASE_URL: process.env.BASE_URL
     }
 
 
