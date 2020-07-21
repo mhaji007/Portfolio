e7afe3c8deda28ee67d50642ce8262d548c142f7
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-20 20:37:59 -0400

    Add fallback to portfolios details page

--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -5,16 +5,23 @@ import BasePage from '@/components/BasePage';
 import {useGetUser} from '@/actions/user';
 import { formatDate } from 'helpers/functions';
 import PortfolioApi from '@/lib/api/portfolios';
+import  {useRouter} from 'next/router';
 
 
 const Portfolio = ({portfolio}) => {
+
+    const router = useRouter();
     
     // const {data:portfolio, error, loading} = useGetUser(); 
     // useGetData(router.query.id?`/api/v1/posts/${router.query.id}`:null); // -  used if not using SWR
     // const {data: portfolio, error, loading} =
     // useGetPostById(router.query.id);
    const {data:dataU, loading:loadingU} = useGetUser();
-    
+   
+   if (router.isFallback) {
+       return <h1> Your page is getting served </h1>
+   }
+
     return (
         <BaseLayout
         navClass="transparent"
@@ -27,13 +34,27 @@ const Portfolio = ({portfolio}) => {
        <div className="portfolio-detail">
           <div  className="cover-container d-flex h-100 p-3 mx-auto flex-column">
             <main role="main"  className="inner page-cover">
+            {
+                router.isFallback &&
+                
+            <h1  className="cover-heading">your page is getting served...</h1>
+            }
+            
+            {!router.isFallback &&
+            
+            <>
             <h1  className="cover-heading">{portfolio.title}</h1>
               <p  className="lead dates">{formatDate(portfolio.startDate)} - {formatDate(portfolio.endDate) || 'Present'}</p>
               <p  className="lead info mb-0">{portfolio.jobTitle} | {portfolio.company} | {portfolio.location}</p>
               <p  className="lead">{portfolio.description}</p>
+              
               <p className="lead">
               <a href={portfolio.companyWebsite} target="_" className="btn btn-lg btn-secondary">Visit Company</a>
               </p>
+
+            </>
+            
+            }
             </main>
           </div>
         </div>
@@ -73,7 +94,7 @@ export async function getStaticPaths() {
         }
     })
     // fallback: false means that 'not found' pages will resolve into 404 page
-    return {paths, fallback:false};
+    return {paths, fallback:true};
 }
 
 export async function getStaticProps({params}) {
